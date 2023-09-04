e16d46f54f69fc9af00b670e4a1febdac4b9e691
Beilei Huang <41132767+bravebeaver@users.noreply.github.com>
2023-09-04 21:58:16 +1200

    use kustomize as runCommand supprot it

--- AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/admin-managed-clusters.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/admin-managed-clusters.service.ts
@@ -7,15 +7,18 @@ import { ArmService } from '../../shared/services/arm.service';
 
 import { CredentialResult, CredentialResults, KubeConfigCredentials, RunCommandRequest, RunCommandResult } from 'projects/diagnostic-data/src/lib/models/managed-cluster-rest';
 import { ManagedCluster, PeriscopeConfig} from '../../shared/models/managed-cluster';
+import { environment } from 'projects/app-service-diagnostics/src/environments/environment';
+
 import { ManagedClustersService } from './managed-clusters.service';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import  * as JSZip  from 'jszip';
+
 
 import * as yaml from 'yaml';
 
 const RUN_COMMAND_INITIAL_POLL_WAIT_MS: number = 1000;
 const RUN_COMMAND_INTERVAL_MS: number = 5000;
-
+const YAML_SEPARATOR: string = "---";
 @Injectable()
 export class AdminManagedClustersService {
   // the admin client exec kubectl command in cluster, need cluster token and not broadcast to other components
@@ -47,6 +50,7 @@ export class AdminManagedClustersService {
     });
   }
 
+  // kubectl does not work, however just keep it here??
   runKubectlPeriscope(periscopeConfig: PeriscopeConfig): Observable<string> {
     // GET https://${apiServerEndpoint}/api/v1/nodes --cacert ca.crt --cert client.crt --key client.key
     const currentCredential =  this.clusterCredentials$.getValue();
@@ -138,6 +142,56 @@ export class AdminManagedClustersService {
     }));    
   }
 
+  runCommandKustomizePeriscope(periscopeConfig: PeriscopeConfig): Observable<RunCommandResult> {
+
+    return this.runCommandInCluster(`${InClusterDiagnosticCommands.APPLY_KUSTOMIZE} ${RunCommandContextConfig.PERISCOPE_CONTEXT}`, 
+    this.createPeriscopeContextUsingKustomize(periscopeConfig));
+  }
+
+  createOverlayDir(fileContent: string): Observable<string> {
+    return of("");
+  }
+
+  createPeriscopeContextUsingKustomize(periscopeConfig: PeriscopeConfig): string {
+    const imageTag = "0.0.13";
+
+    const storageConfig = {
+      storageAccountName: process.env.STORAGE_ACCOUNT_NAME,
+      containerName: process.env.BLOB_CONTAINER_NAME,
+      sasKey: process.env.SAS_KEY,
+    };
+
+    const kustomizeConfig = `
+    resources:
+    - https://github.com/azure/aks-periscope//deployment/base?ref=<RELEASE_TAG>
+    
+    images:
+    - name: periscope-linux
+      newName: mcr.microsoft.com/aks/periscope
+      newTag: ${imageTag}
+    - name: periscope-windows
+      newName: mcr.microsoft.com/aks/periscope
+      newTag: ${imageTag}
+    
+    secretGenerator:
+    - name: azureblob-secret
+      behavior: replace
+      literals:
+      - AZURE_BLOB_ACCOUNT_NAME=${storageConfig.storageAccountName}
+      - AZURE_BLOB_CONTAINER_NAME=${storageConfig.containerName}
+      - AZURE_BLOB_SAS_KEY=${storageConfig.sasKey}
+    
+    # Commented-out config values are the defaults. Uncomment to change.
+    configMapGenerator:
+    - name: diagnostic-config
+      behavior: merge
+      literals:
+      - DIAGNOSTIC_RUN_ID=${periscopeConfig.diagnosticRunId}   
+    `;
+    return kustomizeConfig;
+  }
+
+  // TODO this is rather silly, find another way to interact with kustomize
   createPeriscopeContext(periscopeConfig: PeriscopeConfig): Observable<string> {
     const createNS = `
 apiVersion: v1
@@ -146,7 +200,42 @@ metadata:
   name: aks-periscope
 `;
 
-    return this.convertStringToBase64(createNS, RunCommandContextConfig.PERISCOPE_CONTEXT);
+const createClusterRole = `
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: aks-periscope-role
+rules:
+- apiGroups: ["","metrics.k8s.io"]
+  resources: ["pods", "nodes"]
+  verbs: ["get", "watch", "list"]
+- apiGroups: [""]
+  resources: ["secrets"]
+  verbs: ["list"]
+- apiGroups: [""]
+  resources: ["pods/portforward"]
+  verbs: ["create"]
+- apiGroups: ["aks-periscope.azure.github.com"]
+  resources: ["diagnostics"]
+  verbs: ["get", "watch", "list", "create", "patch"]
+- apiGroups: ["apiextensions.k8s.io"]
+  resources: ["customresourcedefinitions"]
+  verbs: ["get", "list", "watch"]
+- apiGroups: ["access.smi-spec.io", "specs.smi-spec.io", "split.smi-spec.io"]
+  resources: ["*"]
+  verbs: ["get", "list", "watch"]
+- apiGroups: ["config.openservicemesh.io"]
+  resources: ["meshconfigs"]
+  verbs: ["get", "list"]
+- apiGroups: ["admissionregistration.k8s.io"]
+  resources: ["mutatingwebhookconfigurations", "validatingwebhookconfigurations"]
+  verbs: ["get", "list"]
+`;
+
+    // const nsObject = yaml.parseDocument(createNS);
+    // const clusterRoleObject = yaml.parseDocument(createClusterRole);
+    const persicopeManifest =[createNS, createClusterRole].join(YAML_SEPARATOR);
+    return this.convertStringToBase64(persicopeManifest, RunCommandContextConfig.PERISCOPE_CONTEXT);
   }
 
 
@@ -166,6 +255,8 @@ export enum InClusterDiagnosticCommands {
   CLUSTER_INFO = "kubectl cluster-info",
   GET_NODE = "kubectl get nodes",
   APPLY_FILES = "kubectl apply -f",
+  APPLY_KUSTOMIZE = "kubectl apply -k",
+
 }
 
 export enum RunCommandContextConfig {
